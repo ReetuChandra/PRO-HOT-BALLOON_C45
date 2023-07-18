6006b3b994295e4894297fbb7facbc4aa28fce93
ReetuChandra <67150474+ReetuChandra@users.noreply.github.com>
2023-07-18 21:45:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+
+
+function preload(){
+bgImg = loadImage("assets/bg.png")
+
+balloonImg = loadAnimation("assets/balloon1.png","assets/balloon2.png","assets/balloon3.png")
+}
+
+function setup(){
+
+}
+
+function draw() {
+        
+}
